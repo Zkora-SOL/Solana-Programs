6e21508be1ebe4b6b4935a733e930c640ad28ce0
Zkora-SOL <Zkorasol@proton.me>
2026-01-02 01:35:27 +0800

    Update lib.rs

--- programs/common/src/lib.rs
+++ programs/common/src/lib.rs
@@ -10,6 +10,42 @@ pub used: bool,
 #[account]
 pub struct CommitmentRoot {
 pub root: [u8; 32],
+pub index: u64,
+}
+
+
+#[account]
+pub struct AuthorityConfig {
+pub authority: Pubkey,
+}
+
+
+#[error_code]
+pub enum ZKoraError {
+#[msg("Nullifier already used")]
+NullifierAlreadyUsed,
+
+
+#[msg("Invalid attestation signature")]
+InvalidAttestation,
+
+
+#[msg("Unauthorized")]
+Unauthorized,
+}
+```rust
+use anchor_lang::prelude::*;
+
+
+#[account]
+pub struct NullifierAccount {
+pub used: bool,
+}
+
+
+#[account]
+pub struct CommitmentRoot {
+pub root: [u8; 32],
 }
 
 
